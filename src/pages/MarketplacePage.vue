79c5b47d90f10cf33207e9620b76f89332310acc
Rusic <rusic@fastmail.com>
2025-10-05 13:26:31 +0200

    [master] added plug for marketplace page

--- src/pages/MarketplacePage.vue
+++ src/pages/MarketplacePage.vue
@@ -1,26 +1,166 @@
 <script lang="ts" setup>
-    import MainLayout from '@/components/layouts/MainLayout.vue';
-    import { useCounterStore } from '@/stores/counter.ts';
+import MainLayout from '@/components/layouts/MainLayout.vue';
+import { useCounterStore } from '@/stores/counter.ts';
 
-    const counterStore = useCounterStore();
+const counterStore = useCounterStore();
+
+    const tiers = [
+        {
+            name: 'Hobby',
+            id: 'tier-hobby',
+            href: '#',
+            priceMonthly: '$29',
+            description: "The perfect plan if you're just getting started with our product.",
+            features: [
+                '25 products',
+                'Up to 10,000 subscribers',
+                'Advanced analytics',
+                '24-hour support response time',
+            ],
+            featured: false,
+        },
+        {
+            name: 'Enterprise',
+            id: 'tier-enterprise',
+            href: '#',
+            priceMonthly: '$99',
+            description: 'Dedicated support and infrastructure for your company.',
+            features: [
+                'Unlimited products',
+                'Unlimited subscribers',
+                'Advanced analytics',
+                'Dedicated support representative',
+                'Marketing automations',
+                'Custom integrations',
+            ],
+            featured: true,
+        },
+    ];
 </script>
 
 <template>
     <MainLayout>
-        <h1 class="text-3xl font-bold tracking-tight text-gray-900">Marketplace Page</h1>
-
-        <button
-            class="mx-3 rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm text-white"
-            @click="counterStore.increment()"
-        >
-            Add item
-        </button>
-        <button
-            :disabled="counterStore.count <= 0"
-            class="mx-3 rounded-md bg-pink-400 px-3.5 py-2.5 text-sm text-white disabled:bg-gray-200 disabled:text-gray-400"
-            @click="counterStore.decrement()"
-        >
-            Remove item
-        </button>
+        <div class="relative isolate bg-white px-6 py-24 sm:py-32 lg:px-8">
+            <div
+                aria-hidden="true"
+                class="absolute inset-x-0 -top-3 -z-10 transform-gpu overflow-hidden px-36 blur-3xl"
+            >
+                <div
+                    class="mx-auto aspect-1155/678 w-288.75 bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-30"
+                    style="
+                        clip-path: polygon(
+                            74.1% 44.1%,
+                            100% 61.6%,
+                            97.5% 26.9%,
+                            85.5% 0.1%,
+                            80.7% 2%,
+                            72.5% 32.5%,
+                            60.2% 62.4%,
+                            52.4% 68.1%,
+                            47.5% 58.3%,
+                            45.2% 34.5%,
+                            27.5% 76.7%,
+                            0.1% 64.9%,
+                            17.9% 100%,
+                            27.6% 76.8%,
+                            76.1% 97.7%,
+                            74.1% 44.1%
+                        );
+                    "
+                />
+            </div>
+            <div class="mx-auto max-w-4xl text-center">
+                <h2 class="text-base/7 font-semibold text-indigo-600">Pricing</h2>
+                <p
+                    class="mt-2 text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-6xl"
+                >
+                    Choose the right plan for you
+                </p>
+            </div>
+            <p
+                class="mx-auto mt-6 max-w-2xl text-center text-lg font-medium text-pretty text-gray-600 sm:text-xl/8"
+            >
+                Choose an affordable plan that’s packed with the best features for engaging your
+                audience, creating customer loyalty, and driving sales.
+            </p>
+            <div
+                class="mx-auto mt-16 grid max-w-lg grid-cols-1 items-center gap-y-6 sm:mt-20 sm:gap-y-0 lg:max-w-4xl lg:grid-cols-2"
+            >
+                <div
+                    v-for="(tier, tierIdx) in tiers"
+                    :key="tier.id"
+                    :class="[
+                        tier.featured
+                            ? 'relative bg-gray-900 shadow-2xl'
+                            : 'bg-white/60 sm:mx-8 lg:mx-0',
+                        tier.featured
+                            ? ''
+                            : tierIdx === 0
+                              ? 'rounded-t-3xl sm:rounded-b-none lg:rounded-tr-none lg:rounded-bl-3xl'
+                              : 'sm:rounded-t-none lg:rounded-tr-3xl lg:rounded-bl-none',
+                        'rounded-3xl p-8 ring-1 ring-gray-900/10 sm:p-10',
+                    ]"
+                >
+                    <h3
+                        :id="tier.id"
+                        :class="[
+                            tier.featured ? 'text-indigo-400' : 'text-indigo-600',
+                            'text-base/7 font-semibold',
+                        ]"
+                    >
+                        {{ tier.name }}
+                    </h3>
+                    <p class="mt-4 flex items-baseline gap-x-2">
+                        <span
+                            :class="[
+                                tier.featured ? 'text-white' : 'text-gray-900',
+                                'text-5xl font-semibold tracking-tight',
+                            ]"
+                        >
+                            {{ tier.priceMonthly }}
+                        </span>
+                        <span
+                            :class="[
+                                tier.featured ? 'text-gray-400' : 'text-gray-500',
+                                'text-base',
+                            ]"
+                        >
+                            /month
+                        </span>
+                    </p>
+                    <p
+                        :class="[
+                            tier.featured ? 'text-gray-300' : 'text-gray-600',
+                            'mt-6 text-base/7',
+                        ]"
+                    >
+                        {{ tier.description }}
+                    </p>
+                    <ul
+                        :class="[
+                            tier.featured ? 'text-gray-300' : 'text-gray-600',
+                            'mt-8 space-y-3 text-sm/6 sm:mt-10',
+                        ]"
+                        role="list"
+                    >
+                        <li v-for="feature in tier.features" :key="feature" class="flex gap-x-3">
+                            {{ feature }}
+                        </li>
+                    </ul>
+                    <button
+                        :aria-describedby="tier.id"
+                        :class="[
+                            tier.featured
+                                ? 'bg-indigo-500 text-white shadow-xs hover:bg-indigo-400 focus-visible:outline-indigo-500'
+                                : 'text-indigo-600 inset-ring inset-ring-indigo-200 hover:inset-ring-indigo-300 focus-visible:outline-indigo-600',
+                            'mt-8 block rounded-md px-3.5 py-2.5 text-center text-sm font-semibold focus-visible:outline-2 focus-visible:outline-offset-2 sm:mt-10',
+                        ]"
+                        @click="counterStore.increment()"
+                    >
+                        Get started today
+                    </button>
+                </div>
+            </div>
+        </div>
     </MainLayout>
 </template>
