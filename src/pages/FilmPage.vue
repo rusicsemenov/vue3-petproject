765c51652ed0760c89b14c62cb64d0b42aa8cd0a
Rusic <rusic@fastmail.com>
2025-10-05 14:31:59 +0200

    [master] fix: update FilmPage.vue layout and dependencies in package.json (actualized all packages)

--- src/pages/FilmPage.vue
+++ src/pages/FilmPage.vue
@@ -56,7 +56,7 @@ const route = useRoute();
                 class="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 lg:mx-0 lg:max-w-none lg:grid-cols-2 lg:items-start lg:gap-y-10"
             >
                 <div
-                    class="lg:col-span-2 lg:col-start-1 lg:row-start-1 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8"
+                    class="sticky top-0 lg:col-span-2 lg:col-start-1 lg:row-start-1 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8"
                 >
                     <div class="lg:pr-4">
                         <div class="lg:max-w-lg">
